6565f3dab3e5745e7029c4819f3d70d42d733585
Kimutai <jankimsingoei@gmail.com>
2023-11-03 01:14:33 +0300

    css and layout

--- src/components/About.js
+++ src/components/About.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <div className='about-content'>
-      <h2>About BlogBloom</h2>
-      <p>
+    <div className="about-section">
+  <div>
+    <h2>About BlogBloom</h2>
+    <p>
       Welcome to BlogBloom, your go-to destination for discovering and sharing captivating blog posts! Our platform is designed to connect bloggers, writers, and readers in a vibrant community that celebrates the art of storytelling.
-      </p>
-      <h2>
-      What We Offer
-      </h2>
-      <ol>
-        <li>
-          <strong>Diverse Blogging Topics:</strong> BlogBloom covers a wide range of topics, from travel and lifestyle to technology and arts. You'll find blogs that pique your interest and inspire your creativity.
-        </li>
-        <li>
+    </p>
+  </div>
+  <div className="what-we-offer">
+    <h2>What We Offer</h2>
+    <ol>
+      <li>
+        <strong>Diverse Blogging Topics:</strong> BlogBloom covers a wide range of topics, from travel and lifestyle to technology and arts. You'll find blogs that pique your interest and inspire your creativity.
+      </li>
+      <li>
           <strong>Creative Expression:</strong> We believe in the power of creative expression. Our platform encourages bloggers to share their unique stories, perspectives, and experiences with the world.
-        </li>
-        <li>
+      </li>
+      <li>
           <strong>Community Engagement:</strong> Join our community of avid readers, writers, and bloggers. Interact with authors, discuss your favorite posts, and build connections with like-minded individuals.
-        </li>
+      </li>
         <li>
           <strong>Blog Discovery:</strong> Easily discover new and exciting blogs. Use our intuitive search and recommendation features to find content that resonates with your interests.
         </li>
@@ -30,23 +30,22 @@ const About = () => {
         <li>
           <strong>Accessibility:</strong> BlogBloom is accessible from all devices. Whether you prefer reading on your laptop, tablet, or smartphone, we've got you covered.
         </li>
-      </ol>
-      <h2>
-      Our Mission
-      </h2>
-      <p>
+    </ol>
+  </div>
+  <div className="our-mission">
+    <h2>Our Mission</h2>
+    <p>
       At BlogBloom, our mission is to foster a vibrant and inclusive blogging community. We strive to empower bloggers to share their stories, ideas, and passions with a global audience. We believe that everyone has a unique voice, and through blogging, we can connect, learn, and grow together.
-      </p>
-      <h2>
-      Get Started
-      </h2>
-      <p>
+    </p>
+  </div>
+  <div className="get-started">
+    <h2>Get Started</h2>
+    <p>
       Join BlogBloom today and start your journey into the world of blogging. Whether you're a seasoned blogger or a newcomer, you'll find a welcoming community ready to embrace your ideas and stories.
-
       Let your creativity bloom with BlogBloom!
-      </p>
-      </div>
-    </div>
+    </p>
+  </div>
+</div>
   );
 };
 
