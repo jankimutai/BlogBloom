5cb3c4ef56044dfa4c169d11fb93256cd8ef4bae
Kimutai <jankimsingoei@gmail.com>
2023-11-07 15:14:45 +0300

    Deployement changes

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
@@ -7,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 db=SQLAlchemy()
